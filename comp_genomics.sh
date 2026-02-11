84fcc8c0a6efa593a2eb5c26df8885901539d068
carternewt <150153088+carternewt@users.noreply.github.com>
2026-02-11 17:20:45 -0500

    Create comp_genomics.sh & accessions_ID.csv: initial test at establishing script to download Paenibacillus genomes

---
+++ comp_genomics.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#SBATCH --partition=batch
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=8
+#SBATCH --mem=24G
+#SBATCH --time=1:00:00
+#SBATCH --mail-user=carter.newton@uga.edu
+#SBATCH --mail-type=START,END,FAIL
+#SBATCH --error=/work/lylab/cjn40747/comp_genomics/logs/%j.err
+#SBATCH --output=/work/lylab/cjn40747/comp_genomics/logs/%j.out
+
+ml ncbi-genome-download/0.3.3-GCCcore-12.3.0
+
+$OUT='/work/lylab/cjn40747/UV_comp'
+$HOME='/home/cjn40747/UV_Mutagenesis'
+
+mkdir -p $OUT/orig_genome
+ncbi-genome-download -A $HOME/accessions_ID.csv -o $OUT/orig_genome
