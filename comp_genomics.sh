3a0fce3b4afac50e96dada518ff6925b1587bb17
carternewt <150153088+carternewt@users.noreply.github.com>
2026-02-20 09:36:23 -0500

    Update comp_genomics.sh: CheckM2 was downloaded by GACRC. Implementing code using this module

--- comp_genomics.sh
+++ comp_genomics.sh
@@ -12,7 +12,7 @@
 ml ncbi-genome-download/0.3.3-GCCcore-12.3.0
 ml bakta/1.11
 ml QUAST/5.2.0
-ml CheckM/1.2.2-foss-2022a
+ml CheckM2/1.1.0-foss-2024a
 
 OUT='/work/lylab/cjn40747/UV_comp'
 HOME='/home/cjn40747/UV_Mutagenesis'
@@ -30,13 +30,6 @@ find $OUT/orig_genome/refseq/bacteria -name GCF*.gz -type f | while read -r file
     bakta --db $OUT/bakta/db --verbose --output $out_dir --prefix $name --genus Paenibacillus --threads 8 "$file" --force
 done
 
-mkdir -p $OUT/QC
-mkdir -p $OUT/QC/genomes
-find $OUT/orig_genome/refseq/bacteria -name GCF*.gz -type f | while read -r file; do
-    out=$(basename "${file%.gz}")
-    gunzip -c "$file" > $OUT/QC/genomes/$out
-done
-
 find $OUT/orig_genome/refseq/bacteria -name GCF*.gz -type f | while read -r file; do
     dir=$(dirname "$file")
     out_dir="$OUT/QC/$(basename "$dir")"
@@ -44,7 +37,16 @@ find $OUT/orig_genome/refseq/bacteria -name GCF*.gz -type f | while read -r file
     quast -o $out_dir -b -t 8 "$file"
 done
 
-ml purge
-source activate /home/cjn40747/miniconda/envs/checkm2
+mkdir -p $OUT/QC
+mkdir -p $OUT/QC/genomes
+find $OUT/orig_genome/refseq/bacteria -name GCF*.gz -type f | while read -r file; do
+    out=$(basename "${file%.gz}")
+    gunzip -c "$file" > $OUT/QC/genomes/$out
+done
+
+if [ ! -f $OUT/CheckM2_database/uniref100.KO.1.dmnd ]; then
+    checkm2 database --download $OUT
+fi
+
 mkdir -p $OUT/checkm2
-checkm2 predict --threads 8 --input $OUT/QC/genomes --output-directory $OUT/checkm2/results --database_path /work/lylab/cjn40747/F7_5_genome/checkm2/database/CheckM2_database/uniref100.KO.1.dmnd --force
+checkm2 predict --threads 8 --input $OUT/QC/genomes --output-directory $OUT/checkm2/results --database_path $OUT/CheckM2_database/uniref100.KO.1.dmnd --force
