7440efccf6c3c642b366f93925dacd7532eb58c0
carternewt <150153088+carternewt@users.noreply.github.com>
2026-02-24 16:33:08 -0500

    Update comp_genomics.sh: Going to attempt Prokka annotation to pipe into Roary

--- comp_genomics.sh
+++ comp_genomics.sh
@@ -20,9 +20,9 @@ DB="$OUT/CheckM2_database/uniref100.KO.1.dmnd"
 mkdir -p $OUT/orig_genome
 ncbi-genome-download --section refseq --assembly-accessions $ACCESSIONS --output-folder $OUT/orig_genome --formats fasta bacteria
 
-ml purge
-ml bakta/1.11
-mkdir -p $OUT/bakta
+#ml purge
+#ml bakta/1.11
+#mkdir -p $OUT/bakta
 #find $OUT/orig_genome/refseq/bacteria -name GCF*.gz -type f | while read -r file; do
 #    dir=$(dirname "$file")
 #    out_dir="$OUT/bakta/$(basename "$dir")"
@@ -31,8 +31,8 @@ mkdir -p $OUT/bakta
 #    bakta --db $OUT/bakta/db --verbose --output $out_dir --prefix $name --genus Paenibacillus --threads 8 "$file" --force
 #done
 
-ml purge
-ml QUAST/5.2.0
+#ml purge
+#ml QUAST/5.2.0
 #find $OUT/orig_genome/refseq/bacteria -name GCF*.gz -type f | while read -r file; do
 #    dir=$(dirname "$file")
 #    out_dir="$OUT/QC/$(basename "$dir")"
@@ -40,27 +40,38 @@ ml QUAST/5.2.0
 #    quast -o $out_dir -b -t 8 "$file"
 #done
 
-mkdir -p $OUT/QC
-mkdir -p $OUT/QC/genomes
-find $OUT/orig_genome/refseq/bacteria -name GCF*.gz -type f | while read -r file; do
-    out=$(basename "${file%.gz}")
-    gunzip -c "$file" > $OUT/QC/genomes/$out
-done
+#mkdir -p $OUT/QC
+#mkdir -p $OUT/QC/genomes
+#find $OUT/orig_genome/refseq/bacteria -name GCF*.gz -type f | while read -r file; do
+#    out=$(basename "${file%.gz}")
+#    gunzip -c "$file" > $OUT/QC/genomes/$out
+#done
+
+#ml purge
+#ml CheckM2/1.1.0-foss-2024a
+
+#[ -f "$DB" ] || checkm2 database --download --path "$OUT"
+
+#mkdir -p $OUT/checkm2
+#checkm2 predict --threads 8 --input $OUT/QC/genomes --output-directory $OUT/checkm2/results --database_path $DB --force
 
 ml purge
-ml CheckM2/1.1.0-foss-2024a
-
-[ -f "$DB" ] || checkm2 database --download --path "$OUT"
-
-mkdir -p $OUT/checkm2
-#checkm2 predict --threads 8 --input $OUT/QC/genomes --output-directory $OUT/checkm2/results --database_path $DB --force
+ml prokka/1.14.5-gompi-2023a
+mkdir -p $OUT/prokka
+find $OUT/QC/genomes -name *.fna -type f | while read -r file; do
+    dir=$(dirname "$file")
+    out_dir="$OUT/prokka/$(basename "$dir")"
+    name=$(basename "$dir")
+    mkdir -p $out_dir
+    prokka --outdir $out_dir --force --prefix $name --genus Paenibacillus --cpus 8 $file
+done
 
 ml purge
 ml Roary/3.13.0-foss-2022a
 
 mkdir -p $OUT/roary
-find $OUT/bakta -name *.gff3 -type f | while read -r file; do
+find $OUT/prokka -name *.gff -type f | while read -r file; do
     cp $file $OUT/roary
 done
 
-roary -e -n -v -p 8 -f $OUT/roary $OUT/roary/*.gff3
+roary -e -n -v -p 8 -f $OUT/roary $OUT/roary/*.gff
